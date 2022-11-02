06f1361c0f63026de5573f1edf99a77a3d19e56b
Bonsen Wakjira <bonsenw@gmail.com>
2022-11-02 21:49:58 +1100

    Major Refactoring Towards Pomodoro Timer
     - Updated the graphics to suit a minimalistic appeal\n - Reworked the logic to match any changes to GUI (aka removing start and reset buttons)\n - Cleaned up code to reduce clutter\n - Created an external settings JSON file as well as a settings manager to manage the many settings\n - Had a more dynamic canvas that changes with the timer - More bug fixes and GUI updates

---
+++ proj/Pomodoro Timer/data/PomodoroAppSettings.py
@@ -0,0 +1,47 @@
+import json
+import os
+
+class PomodoroAppSettings():
+    
+    MAIN_PATH = "C:\\Users\\bonse\\OneDrive\\Desktop\\Bzone\\#1 Work\\#1 Coding\\Python\\Finished Projects\\Python Beginner Collection\\proj\\Pomodoro Timer"
+    
+    # General
+    TIME_DATA_PATH = f"{MAIN_PATH}\\data\\time.json"
+    SETTING_COG_IMAGE_PATH = f"{MAIN_PATH}\\images\\Settings Cog.png"
+    BG_COLOUR_BLUE = "#00111e"
+    BG_COLOUR_GREEN = "#001e0b"
+    BG_COLOUR_RED = "#1e0005"
+    FONT_NAME = "Screaming Neon"
+
+    # Screen Dimension
+    SCREEN_DIMENSIONS = '500x500'
+    SCREEN_WIDTH = 500
+    SCREEN_HEIGHT = 500
+    
+    # Halo Images
+    GREEN_HALO_IMAGE_PATH = f"{MAIN_PATH}\\images\\Halo Border Green.png"
+    BLUE_HALO_IMAGE_PATH = f"{MAIN_PATH}\\images\\Halo Border Blue.png"
+    RED_HALO_IMAGE_PATH = f"{MAIN_PATH}\\images\\Halo Border Red.png"
+    HALO_WIDTH = 250
+    HALO_HEIGHT = 250
+    
+    # Labels
+    TIMER_TEXT_FONT = (FONT_NAME, 35, 'bold')
+    TIMER_TEXT_COLOUR = "#ffffff"
+    START_TEXT_FONT = (FONT_NAME, 10, 'italic')
+    START_TEXT_COLOUR = "#ffffff"
+    
+    def get_times(self, time_name: str):
+        """ Returns the time for the given time name (work, short_break, long_break) """
+        with open(self.TIME_DATA_PATH, 'r') as data:
+            current_data: dict = json.load(data)
+            assert time_name in current_data.keys(), f"Given key {time_name} is not in dictionary, keys avaliable are: {current_data.keys()}"
+            return current_data[time_name]
+    
+    def change_times(self, time_name: str, new_value: int):
+        with open(self.TIME_DATA_PATH, 'r') as data:
+            current_data: dict = json.load(data)
+            assert time_name in current_data.keys(), f"Given key {time_name} is not in dictionary, keys avaliable are: {current_data.keys()}"
+        with open(self.TIME_DATA_PATH, 'w') as data:
+            current_data[time_name] = new_value
+            json.dump(current_data, data, indent=4)
--- proj/Pomodoro Timer/main.py
+++ proj/Pomodoro Timer/main.py
@@ -1,122 +1,153 @@
-import tkinter as tk
+from data.PomodoroAppSettings import PomodoroAppSettings
 from tkinter import messagebox
+import tkinter as tk
 import datetime
+import winsound
+import math
 
-COLOURS = {
-    "PINK" : "#e2979c",
-    "RED" : "#e7305b",
-    "GREEN" : "#9bdeac",
-    "LY" : "#f7f5dd",
-    "MY" : "#dbd9c3",
-    "DY" : "#bab8a6"
-}
-
-FONT_NAME = "Courier"
-WORK_MIN = 30
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
-
-class App(tk.Tk):
-    
-    current_reps = 0
-    timer = None
-    
+class PomodoroApp(tk.Tk):
     def __init__(self):
         super().__init__()
-        
-        self.title("Pomodoro")
-        self.config(padx=100, pady=50, bg=COLOURS["LY"])
+        # Variables
+        self.repetition_number = 0
+        self.timer_started = False
+        # Configuration
+        self.settings = PomodoroAppSettings()
+        self.setup_screen()
+        self.setup_canvas()
+        self.bind("<KeyRelease>", self.on_space_release)
 
-        # Create Canvas
-        self.canvas = tk.Canvas(width=200, height=224, bg=COLOURS['LY'], highlightthickness=0)
-        self.tomato_img = tk.PhotoImage(master=self.canvas, file="tomato.png")
-        self.canvas.create_image(100, 112, image=self.tomato_img)
-        self.timer_text = self.canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 34, "bold"))
-
-        # Header
-        self.header_label = tk.Label(text="Timer", fg=COLOURS["GREEN"], font=(FONT_NAME, 40, "bold"), bg=COLOURS["LY"])
-
-        # Buttons
-        self.start_button = tk.Button(text="Start", font=(FONT_NAME, 16, "bold"), bg=COLOURS["MY"], activebackground=COLOURS["DY"], border=0, command=self.start_timer)
-        self.reset_button = tk.Button(text="Reset", font=(FONT_NAME, 16, "bold"), bg=COLOURS["MY"], activebackground=COLOURS["DY"], border=0, command=self.reset_timer)
-
-        # Checklists
-        self.checkmark_label = tk.Label(fg=COLOURS["GREEN"], font=(FONT_NAME, 15, "bold"), bg=COLOURS["LY"])
-        
-        # Entries
-        self.work_min_entry = tk.Entry(bg=COLOURS["MY"], bd=0, justify="left")
-        self.work_min_label = tk.Label(text="Work Duration:", font=(FONT_NAME, 8, "bold"), bg=COLOURS["LY"], justify="right")
-        self.short_break_min_entry = tk.Entry(bg=COLOURS["MY"], bd=0, justify="left")
-        self.short_break_min_label = tk.Label(text="Short Break Duration:", font=(FONT_NAME, 8, "bold"), bg=COLOURS["LY"], justify="right")
-        self.long_break_min_entry = tk.Entry(bg=COLOURS["MY"], bd=0, justify="left")
-        self.long_break_min_label = tk.Label(text="Long Break Duration:", font=(FONT_NAME, 8, "bold"), bg=COLOURS["LY"], justify="right")
-        
-        self.layout_ui()
-
-    def layout_ui(self):
-        self.canvas.grid(row=1, column=1)
-        self.header_label.grid(row=0, column=1)
-        self.start_button.grid(row=2,column=0)
-        self.reset_button.grid(row=2,column=3)
-        self.checkmark_label.grid(row=3, column=1)
-        self.work_min_entry.grid(row=4, column=1, pady=1)
-        self.work_min_label.grid(row=4, column=0)
-        self.short_break_min_entry.grid(row=5, column=1, pady=1)
-        self.short_break_min_label.grid(row=5, column=0)
-        self.long_break_min_entry.grid(row=6, column=1, pady=1)
-        self.long_break_min_label.grid(row=6, column=0)
-
-    def reset_timer(self):
-        self.current_reps = 0
-
-        self.after_cancel(self.timer)
-        self.canvas.itemconfig(self.timer_text, text="00:00")
-        self.header_label["text"] = "Timer"
-        self.checkmark_label["text"] = ''
+    def setup_screen(self):
+        """ Initialises the GUI screen. """
+        self.title("Minimalistic Pomodoro Timer")
+        self.geometry(self.settings.SCREEN_DIMENSIONS)
+        self.main_frame = tk.Frame(master=self, width=self.settings.SCREEN_WIDTH, height=self.settings.SCREEN_HEIGHT)
+        self.main_frame.grid(row=0, column=0)
     
-    def start_timer(self):
-        try:
-            work_sec = float(self.work_min_entry.get()) * 60
-            short_break_sec = float(self.short_break_min_entry.get()) * 60
-            long_break_sec = float(self.long_break_min_entry.get()) * 60
-        except ValueError:
-            messagebox.showerror(title="Value Error", message="Invalid format for duration, must be an integer or a floating point number.")
-            return
+    def setup_canvas(self):
+        """ Sets up the base canvas for the timer. """
+        # Variables
+        canvas_width = self.settings.SCREEN_WIDTH 
+        canvas_height = self.settings.SCREEN_HEIGHT
+        center = (canvas_width/2, canvas_height/2)
+        lower_center = (canvas_width/2, canvas_height/2+40)
+        top_right = (10, 10)
+        # Canvas
+        self.canvas = tk.Canvas(master=self.main_frame, width=canvas_width, height=canvas_height, bg=self.settings.BG_COLOUR_BLUE, highlightthickness=0) 
         
-        if self.current_reps == 0:
+        self.halo_img = tk.PhotoImage(master=self.canvas, file=self.settings.BLUE_HALO_IMAGE_PATH)
+        self.canvas.create_image(center, image=self.halo_img)
+        
+        self.cog_img = tk.PhotoImage(master=self.canvas, file=self.settings.SETTING_COG_IMAGE_PATH)
+        self.cog_button = tk.Button(master=self.canvas, image=self.cog_img, bg=self.settings.BG_COLOUR_BLUE, activebackground=self.settings.BG_COLOUR_BLUE, border=0, command=self.open_settings)
+        self.canvas.create_window(top_right, anchor=tk.NW, window=self.cog_button)
+        
+        self.timer_text = self.canvas.create_text(center, text=' '.join([i for i in "TIMER"]), fill=self.settings.TIMER_TEXT_COLOUR, font=self.settings.TIMER_TEXT_FONT)
+        self.start_text = self.canvas.create_text(lower_center, text=' '.join([i for i in "PRESS SPACE TO START TIMER"]), fill=self.settings.START_TEXT_COLOUR, font=self.settings.START_TEXT_FONT)
+        
+        self.canvas.grid(row=0, column=0)
+        
+    def start_timer(self):
+        self.repetition_number += 1
+        work_sec = self.settings.get_times('work_min') * 60
+        short_break_sec = self.settings.get_times('short_break_min') * 60
+        long_break_sec = self.settings.get_times('long_break_min') * 60
+        
+        if self.repetition_number%8 == 0:
+            # Long break
+            self.alternate_gui_colours()
+            winsound.Beep(5000, 1000)
+            self.change_start_text("Break Time! ")
+            self.after(3000, self.change_start_text, f"Press Space To Reset! ")
+            self.count_down(long_break_sec)
+        elif self.repetition_number%2 == 0:
+            # Short break
+            self.alternate_gui_colours()
+            winsound.Beep(5000, 1000)
+            self.change_start_text("Break Time! ")
+            self.after(3000, self.change_start_text, f"Press Space To Reset! ")
+            self.count_down(short_break_sec)
+        else:
+            # Work
+            self.change_start_text("Work Time! ")
+            winsound.Beep(2500, 500)
+            self.after(3000, self.change_start_text, f"{math.ceil((8 - self.repetition_number)/2)} Pomodoros Left! ")
+            self.after(6000, self.change_start_text, f"Press Space To Reset! ")
+            self.alternate_gui_colours()
             self.count_down(work_sec)
-            self.current_reps += 1
-            return
-        else: 
-            self.current_reps += 1
-
-        if self.current_reps % 8 == 0:
-            self.start_countdown(long_break_sec, fg_colour="RED")
-            self.current_reps = 0
-            self.checkmark_label["text"] = ''
-        elif self.current_reps % 2 == 0:
-            self.start_countdown(short_break_sec, fg_colour="PINK")
-        else:
-            self.start_countdown(work_sec)
-  
-    def start_countdown(self, count_down_time, is_break: bool = False, fg_colour: str = "GREEN"):
-        messagebox.showinfo(title="Pomodoro Timer", message="Time is over")
-        if is_break:
-            self.header_label["text"] = "Break"
-            self.checkmark_label["text"] += '✔'
-        else:
-            self.header_label["text"] = "Work"
-
-        self.header_label["fg"] = COLOURS[fg_colour]
-        self.count_down(count_down_time)
-
+    
+    def reset_timer(self):
+        self.repetition_number = 0
+        self.timer_started = False
+        self.after_cancel(self.timer)
+        self.change_gui_colour(bg_colour = self.settings.BG_COLOUR_BLUE, halo_img_path = self.settings.BLUE_HALO_IMAGE_PATH)
+        self.change_timer_text("TIMER")
+        self.change_start_text("PRESS SPACE TO START TIMER")
+         
     def count_down(self, count):
-        self.canvas.itemconfig(self.timer_text, text=str(datetime.timedelta(seconds=count))[2::])
+        new_time = str(datetime.timedelta(seconds=count))[2::]
+        self.change_timer_text(new_time)
         if count >= 0:
             self.timer = self.after(1000, self.count_down, count - 1)
         else:
             self.start_timer()
 
+    def open_settings(self):
+        newWindow = tk.Toplevel(master=self, bg=self.settings.BG_COLOUR_BLUE)
+        newWindow.title("Pomodoro Timer Settings")
+        tk.Label(master=newWindow, text="Work Minutes").grid(column=0, row=0, padx=5, pady=5)
+        work_min_entry = tk.Entry(master=newWindow)
+        work_min_entry.grid(column=0, row=1, padx=5, pady=5)
+        tk.Label(master=newWindow, text="Short Break Minutes").grid(column=0, row=2, padx=5, pady=5)
+        short_break_min_entry = tk.Entry(newWindow)
+        short_break_min_entry.grid(column=0, row=3, padx=5, pady=5)
+        tk.Label(master=newWindow, text="Long Break Minutes").grid(column=0, row=4, padx=5, pady=5)
+        long_break_min_entry = tk.Entry(master=newWindow)
+        long_break_min_entry.grid(column=0, row=5, padx=5, pady=5)
+        
+        def change_time_settings():
+            try:
+                self.settings.change_times('work_min', new_value=int(work_min_entry.get()))
+                self.settings.change_times('short_break_min', new_value=int(short_break_min_entry.get()))
+                self.settings.change_times('long_break_min', new_value=int(long_break_min_entry.get()))
+            except ValueError:
+                messagebox.showerror(title="VALUE ERROR!", message="Invalid Value")
+        
+        confirm_button = tk.Button(master=newWindow, text="Confirm", command=change_time_settings)
+        confirm_button.grid(column=0, row=6, padx=5, pady=10)
+        
+    def change_timer_text(self, new_text):
+        self.canvas.itemconfig(self.timer_text, text=' '.join([i for i in new_text]))
+    
+    def change_start_text(self, new_text):
+        self.canvas.itemconfig(self.start_text, text=' '.join([i for i in new_text]))
+
+    def change_gui_colour(self, bg_colour, halo_img_path):
+        center = (self.settings.SCREEN_WIDTH /2, self.settings.SCREEN_HEIGHT/2)
+        self.canvas['bg'] = bg_colour
+        self.cog_button['bg'] = bg_colour
+        self.halo_img = tk.PhotoImage(master=self.canvas, file=halo_img_path)
+        self.canvas.create_image(center, image=self.halo_img)
+        
+    def alternate_gui_colours(self):
+        bg_red = self.settings.BG_COLOUR_RED
+        bg_green = self.settings.BG_COLOUR_GREEN
+        
+        if self.canvas['bg'] == bg_red:
+            self.change_gui_colour(bg_colour=bg_green, halo_img_path=self.settings.GREEN_HALO_IMAGE_PATH)
+        else:
+            self.change_gui_colour(bg_colour=bg_red, halo_img_path=self.settings.RED_HALO_IMAGE_PATH)
+
+    def on_space_release(self, e):
+        """ Starts the timer on space key release. """
+        space_key_code = '32'
+        if str(e)[50:52] == space_key_code:
+            if self.timer_started == False:
+                self.timer_started = True
+                self.start_timer()
+            elif self.timer_started == True:
+                self.reset_timer()
+
 if __name__ == "__main__":
-    app = App()
+    app = PomodoroApp()
     app.mainloop()
